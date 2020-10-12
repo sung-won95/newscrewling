350fb257a599217c886ebc81d64a30273b136537
slowlove1995 <parksw1995@naver.com>
2020-10-12 10:01:09 +0900

    feat: extraction keyword to news title

--- src/jungAngNewsCrawling/jungAngNewsCrawling/items.py
+++ src/jungAngNewsCrawling/jungAngNewsCrawling/items.py
@@ -10,4 +10,5 @@ class JungangnewscrawlingItem(scrapy.Item):
     # define the fields for your item here like:
     title = scrapy.Field()
     url = scrapy.Field()
+    keyword = scrapy.Field()
     pass
